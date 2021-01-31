0146f5458990e24056852e64a219ca673d194849
Luis Pool <pool.luis@gmail.com>
2021-01-30 23:28:01 -0600

    Applying changes to the project configuration

--- src/Core3_1/CompanyEmployees/Startup.cs
+++ src/Core3_1/CompanyEmployees/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace CompanyEmployees
 {
@@ -25,6 +19,10 @@ namespace CompanyEmployees
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Custom Configured Services
+            services.ConfigureCors();
+            services.ConfigureIISIntegration();
+
             services.AddControllers();
         }
 
@@ -37,6 +35,13 @@ namespace CompanyEmployees
             }
 
             app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseCors("CorsPolicy");
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.All
+            });
 
             app.UseRouting();
 
