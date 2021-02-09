b9e05ff0c54474d4a190c40e93b7d690664ff461
Luis Pool <pool.luis@gmail.com>
2021-02-09 01:32:19 -0600

    Make the project ready to compile

--- src/Core3_1/LoggerService/LoggerManager.cs
+++ src/Core3_1/LoggerService/LoggerManager.cs
@@ -1,7 +1,29 @@
+using NLog;
+
 namespace LoggerService
 {
     public class LoggerManager : ILoggerManager
     {
         private static ILogger logger = LogManager.GetCurrentClassLogger();
+
+        public void LogDebug(string message)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void LogError(string message)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void LogInfo(string message)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void LogWarn(string message)
+        {
+            throw new System.NotImplementedException();
+        }
     }   
 }
